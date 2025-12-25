d6fd543576d05a7091a44c27e7a448a0db92d09e
flackandreas <flack.andreas@gmail.com>
2025-12-25 08:59:20 +0100

    feat: Initialize Symfony project with Teacher CRUD, testing, and Docker configuration.

---
+++ src/Entity/Teacher.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeacherRepository::class)]
+class Teacher
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2552)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $shortcode = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShortcode(): ?string
+    {
+        return $this->shortcode;
+    }
+
+    public function setShortcode(string $shortcode): static
+    {
+        $this->shortcode = $shortcode;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
